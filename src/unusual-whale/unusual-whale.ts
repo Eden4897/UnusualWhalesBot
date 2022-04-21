27ddd76728d62212bf69cd28e7efc99cf78114c7
Eden <cheungedenhk@gmail.com>
2022-04-21 23:04:52 +0800

    enable commands

--- src/unusual-whale/unusual-whale.ts
+++ src/unusual-whale/unusual-whale.ts
@@ -67,173 +67,173 @@ export async function startScraper(): Promise<void> {
   const loginPage: puppeteer.Page = await login(browser);
 
   await Promise.all([
-    // //Intraday Analyst
-    // new Promise<void>(async (res) => {
-    //   pages.intradayAnalyst = await browser.newPage();
-    //   await pages.intradayAnalyst.setUserAgent(
-    //     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4298.0 Safari/537.36"
-    //   );
-    //   await pages.intradayAnalyst.setViewport({ width: 2560, height: 2000 });
-    //   res();
-    // }),
+    //Intraday Analyst
+    new Promise<void>(async (res) => {
+      pages.intradayAnalyst = await browser.newPage();
+      await pages.intradayAnalyst.setUserAgent(
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4298.0 Safari/537.36"
+      );
+      await pages.intradayAnalyst.setViewport({ width: 2560, height: 2000 });
+      res();
+    }),
 
-    // //Hot Chains and Tickers
-    // new Promise<void>(async (res) => {
-    //   pages.hotChainsNTickers = await browser.newPage();
-    //   await pages.hotChainsNTickers.setUserAgent(
-    //     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4298.0 Safari/537.36"
-    //   );
-    //   await pages.hotChainsNTickers.setViewport({ width: 2560, height: 4000 });
-    //   await pages.hotChainsNTickers.goto(
-    //     "https://www.unusualwhales.com/flow/hot_chains"
-    //   );
-    //   await new Promise((res) => setTimeout(res, 1000));
-    //   res();
-    // }),
+    //Hot Chains and Tickers
+    new Promise<void>(async (res) => {
+      pages.hotChainsNTickers = await browser.newPage();
+      await pages.hotChainsNTickers.setUserAgent(
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4298.0 Safari/537.36"
+      );
+      await pages.hotChainsNTickers.setViewport({ width: 2560, height: 4000 });
+      await pages.hotChainsNTickers.goto(
+        "https://www.unusualwhales.com/flow/hot_chains"
+      );
+      await new Promise((res) => setTimeout(res, 1000));
+      res();
+    }),
 
-    // //Hot Chains and Tickers Hidden
-    // new Promise<void>(async (res) => {
-    //   pages.hotChainsNTickersHidden = await browser.newPage();
-    //   await pages.hotChainsNTickersHidden.setUserAgent(
-    //     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4298.0 Safari/537.36"
-    //   );
-    //   await pages.hotChainsNTickersHidden.setViewport({
-    //     width: 2560,
-    //     height: 4000,
-    //   });
-    //   await pages.hotChainsNTickersHidden.goto(
-    //     "https://www.unusualwhales.com/flow/hot_chains"
-    //   );
-    //   await pages.hotChainsNTickersHidden.waitForSelector(
-    //     ".MuiButton-contained"
-    //   );
-    //   await pages.hotChainsNTickersHidden.click(".MuiButton-contained");
-    //   await new Promise((res) => setTimeout(res, 1000));
-    //   res();
-    // }),
+    //Hot Chains and Tickers Hidden
+    new Promise<void>(async (res) => {
+      pages.hotChainsNTickersHidden = await browser.newPage();
+      await pages.hotChainsNTickersHidden.setUserAgent(
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4298.0 Safari/537.36"
+      );
+      await pages.hotChainsNTickersHidden.setViewport({
+        width: 2560,
+        height: 4000,
+      });
+      await pages.hotChainsNTickersHidden.goto(
+        "https://www.unusualwhales.com/flow/hot_chains"
+      );
+      await pages.hotChainsNTickersHidden.waitForSelector(
+        ".MuiButton-contained"
+      );
+      await pages.hotChainsNTickersHidden.click(".MuiButton-contained");
+      await new Promise((res) => setTimeout(res, 1000));
+      res();
+    }),
 
-    // //Ticker Flows
-    // new Promise<void>(async (res) => {
-    //   pages.tickerFlows = await browser.newPage();
-    //   await pages.tickerFlows.setUserAgent(
-    //     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4298.0 Safari/537.36"
-    //   );
-    //   await pages.tickerFlows.setViewport({ width: 2560, height: 3000 });
-    //   await pages.tickerFlows.goto(
-    //     "https://www.unusualwhales.com/flow/ticker_flows"
-    //   );
-    //   res();
-    // }),
+    //Ticker Flows
+    new Promise<void>(async (res) => {
+      pages.tickerFlows = await browser.newPage();
+      await pages.tickerFlows.setUserAgent(
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4298.0 Safari/537.36"
+      );
+      await pages.tickerFlows.setViewport({ width: 2560, height: 3000 });
+      await pages.tickerFlows.goto(
+        "https://www.unusualwhales.com/flow/ticker_flows"
+      );
+      res();
+    }),
 
-    // //Chain Explorer
-    // new Promise<void>(async (res) => {
-    //   pages.chainExplorer = await browser.newPage();
-    //   await pages.chainExplorer.setUserAgent(
-    //     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4298.0 Safari/537.36"
-    //   );
-    //   await pages.chainExplorer.setViewport({ width: 2560, height: 1440 });
-    //   await pages.chainExplorer.goto(
-    //     "https://www.unusualwhales.com/flow/option_chains"
-    //   );
-    //   res();
-    // }),
+    //Chain Explorer
+    new Promise<void>(async (res) => {
+      pages.chainExplorer = await browser.newPage();
+      await pages.chainExplorer.setUserAgent(
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4298.0 Safari/537.36"
+      );
+      await pages.chainExplorer.setViewport({ width: 2560, height: 1440 });
+      await pages.chainExplorer.goto(
+        "https://www.unusualwhales.com/flow/option_chains"
+      );
+      res();
+    }),
 
-    // //Sector Flow
-    // new Promise<void>(async (res) => {
-    //   pages.sectorFlow = await browser.newPage();
-    //   await pages.sectorFlow.setUserAgent(
-    //     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4298.0 Safari/537.36"
-    //   );
-    //   await pages.sectorFlow.setViewport({ width: 2560, height: 1500 });
-    //   await pages.sectorFlow.goto("https://www.unusualwhales.com/flow/sectors");
-    //   res();
-    // }),
+    //Sector Flow
+    new Promise<void>(async (res) => {
+      pages.sectorFlow = await browser.newPage();
+      await pages.sectorFlow.setUserAgent(
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4298.0 Safari/537.36"
+      );
+      await pages.sectorFlow.setViewport({ width: 2560, height: 1500 });
+      await pages.sectorFlow.goto("https://www.unusualwhales.com/flow/sectors");
+      res();
+    }),
 
-    // //Open Intrest Explorer
-    // new Promise<void>(async (res) => {
-    //   pages.openIntrestExplorer = await browser.newPage();
-    //   await pages.openIntrestExplorer.setUserAgent(
-    //     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4298.0 Safari/537.36"
-    //   );
-    //   await pages.openIntrestExplorer.setViewport({
-    //     width: 2560,
-    //     height: 2000,
-    //   });
-    //   await pages.openIntrestExplorer.goto(
-    //     "https://www.unusualwhales.com/flow/oi_changes"
-    //   );
-    //   res();
-    // }),
+    //Open Intrest Explorer
+    new Promise<void>(async (res) => {
+      pages.openIntrestExplorer = await browser.newPage();
+      await pages.openIntrestExplorer.setUserAgent(
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4298.0 Safari/537.36"
+      );
+      await pages.openIntrestExplorer.setViewport({
+        width: 2560,
+        height: 2000,
+      });
+      await pages.openIntrestExplorer.goto(
+        "https://www.unusualwhales.com/flow/oi_changes"
+      );
+      res();
+    }),
 
-    // //Options Dashboard
-    // new Promise<void>(async (res) => {
-    //   pages.optionsDashboard = await browser.newPage();
-    //   await pages.optionsDashboard.setUserAgent(
-    //     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4298.0 Safari/537.36"
-    //   );
-    //   await pages.optionsDashboard.setViewport({
-    //     width: 2560,
-    //     height: 4500,
-    //   });
-    //   await pages.optionsDashboard.goto(
-    //     "https://www.unusualwhales.com/flow/dashboard"
-    //   );
-    //   res();
-    // }),
+    //Options Dashboard
+    new Promise<void>(async (res) => {
+      pages.optionsDashboard = await browser.newPage();
+      await pages.optionsDashboard.setUserAgent(
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4298.0 Safari/537.36"
+      );
+      await pages.optionsDashboard.setViewport({
+        width: 2560,
+        height: 4500,
+      });
+      await pages.optionsDashboard.goto(
+        "https://www.unusualwhales.com/flow/dashboard"
+      );
+      res();
+    }),
 
-    // //Options Dashboard Hidden
-    // new Promise<void>(async (res) => {
-    //   pages.optionsDashboardHidden = await browser.newPage();
-    //   await pages.optionsDashboardHidden.setUserAgent(
-    //     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4298.0 Safari/537.36"
-    //   );
-    //   await pages.optionsDashboardHidden.setViewport({
-    //     width: 2560,
-    //     height: 4500,
-    //   });
-    //   await pages.optionsDashboardHidden.goto(
-    //     "https://www.unusualwhales.com/flow/dashboard"
-    //   );
-    //   await new Promise((res) => setTimeout(res, 1000));
-    //   await pages.optionsDashboardHidden.evaluate(() => {
-    //     Array(
-    //       ...document.querySelectorAll(".MuiButton-containedPrimary")
-    //     ).forEach((button) => {
-    //       (<HTMLElement>button).click();
-    //     });
-    //   });
-    //   res();
-    // }),
+    //Options Dashboard Hidden
+    new Promise<void>(async (res) => {
+      pages.optionsDashboardHidden = await browser.newPage();
+      await pages.optionsDashboardHidden.setUserAgent(
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4298.0 Safari/537.36"
+      );
+      await pages.optionsDashboardHidden.setViewport({
+        width: 2560,
+        height: 4500,
+      });
+      await pages.optionsDashboardHidden.goto(
+        "https://www.unusualwhales.com/flow/dashboard"
+      );
+      await new Promise((res) => setTimeout(res, 1000));
+      await pages.optionsDashboardHidden.evaluate(() => {
+        Array(
+          ...document.querySelectorAll(".MuiButton-containedPrimary")
+        ).forEach((button) => {
+          (<HTMLElement>button).click();
+        });
+      });
+      res();
+    }),
 
-    // //Options Charting
-    // new Promise<void>(async (res) => {
-    //   pages.optionsCharting = await browser.newPage();
-    //   await pages.optionsCharting.setUserAgent(
-    //     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4298.0 Safari/537.36"
-    //   );
-    //   await pages.optionsCharting.setViewport({
-    //     width: 2560,
-    //     height: 2500,
-    //   });
-    //   await pages.optionsCharting.goto(
-    //     "https://www.unusualwhales.com/flow/charting"
-    //   );
-    //   res();
-    // }),
+    //Options Charting
+    new Promise<void>(async (res) => {
+      pages.optionsCharting = await browser.newPage();
+      await pages.optionsCharting.setUserAgent(
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4298.0 Safari/537.36"
+      );
+      await pages.optionsCharting.setViewport({
+        width: 2560,
+        height: 2500,
+      });
+      await pages.optionsCharting.goto(
+        "https://www.unusualwhales.com/flow/charting"
+      );
+      res();
+    }),
 
-    // //Historical Flow
-    // new Promise<void>(async (res) => {
-    //   pages.historicalFlow = await browser.newPage();
-    //   await pages.historicalFlow.setUserAgent(
-    //     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4298.0 Safari/537.36"
-    //   );
-    //   await pages.historicalFlow.setViewport({
-    //     width: 2560,
-    //     height: 2500,
-    //   });
-    //   await pages.historicalFlow.goto("https://www.unusualwhales.com/flow/");
-    //   res();
-    // }),
+    //Historical Flow
+    new Promise<void>(async (res) => {
+      pages.historicalFlow = await browser.newPage();
+      await pages.historicalFlow.setUserAgent(
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4298.0 Safari/537.36"
+      );
+      await pages.historicalFlow.setViewport({
+        width: 2560,
+        height: 2500,
+      });
+      await pages.historicalFlow.goto("https://www.unusualwhales.com/flow/");
+      res();
+    }),
     watchFlow(browser),
     watchNewsFlow(browser),
     watchFlowAlert(browser),
